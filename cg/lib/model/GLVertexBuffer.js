496da33d943b5dcc7455131980fd9c2a58009d82
zhaomeng.oh <zhaomeng.oh@bytedance.com>
2021-07-20 13:14:49 +0800

    update

--- cg/lib/model/GLVertexBuffer.js
+++ cg/lib/model/GLVertexBuffer.js
@@ -2,8 +2,6 @@
 import RenderContext from '../RenderContext'
 export default class GLVertexBuffer {
 
-  // dimension： 纬度
-  // mode：static dynamic
   constructor(name, data, dimension = 3, mode) {
     
     this.dimension = dimension
@@ -20,25 +18,23 @@ export default class GLVertexBuffer {
     this.data = data
 
   }
-  // 关联
 
-  associate() {
+  associate(){
     this.location = 
       this.gl.getAttribLocation(RenderContext.getProgram(), this.name)
     this.gl.enableVertexAttribArray(this.location)
-    // this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffer)
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.buffer)
     const type = this.gl.FLOAT
     const normalized = false
     const stride = 0
     const offset = 0
-    // glsl 
     this.gl.vertexAttribPointer(
       this.location,
       this.dimension,
       type,
       normalized,
-      stride,  // 间隔
-      offset   // 
+      stride,
+      offset
     )
   }
 
--- cg/lib/model/Mesh.js
+++ cg/lib/model/Mesh.js
@@ -5,11 +5,8 @@ import GLVertexBuffer from './GLVertexBuffer'
 export class Mesh {
 
   constructor({vertices, indices = null, dimension = 3, colors = null, texCoords, norms}){
-    // 纬度
     this.dimension = dimension
-    // 顶点数据
     this.vertices = vertices
-    // 索引
     this.indices = indices
     this.colors = colors
     this.norms = norms
@@ -68,13 +65,8 @@ export class Mesh {
   draw(){
     const gl = this.gl
 
-    // 顶点
-    // 将顶点数据 写入缓冲区
-    // 取到内存地址 绑定 js 变量
     this.vertexBuffer.associate()
-    // 颜色
     this.colorsBuffer && this.colorsBuffer.associate()
-    // 索引
     this.indicesBuffer && this.indicesBuffer.associate()
     this.texCoords && this.texturesBuffer.associate()
     this.normsBuffer && this.normsBuffer.associate()
